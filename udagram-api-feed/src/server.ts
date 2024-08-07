7ddb9c2e83a10c576ddf5c1b7b005d339b8e3302
Minh Sơn <tranminhson456@gmail.com>
2024-08-07 22:20:03 +0700

    piline

--- udagram-api-feed/src/server.ts
+++ udagram-api-feed/src/server.ts
@@ -19,9 +19,9 @@ import {V0_FEED_MODELS} from './controllers/v0/model.index';
 
   app.use(bodyParser.json());
 
-  // We set the CORS origin to * so that we don't need to
+  // We set the CORS origin to * so that we don't need t
   // worry about the complexities of CORS this lesson. It's
-  // something that will be covered in the next course.
+  // something that will be covered in the next course .
   app.use(cors({
     allowedHeaders: [
       'Origin', 'X-Requested-With',
